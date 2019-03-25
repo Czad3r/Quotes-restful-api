0c2bb7f37ca0b747e3ad397bfd91784ff0213f57
czader98 <czader98@poczta.fm>
2019-03-25 20:57:46 +0100

    Initial commit

---
+++ src/main/java/pl/kowalczuk/quotesrestfulapi/QuotesRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package pl.kowalczuk.quotesrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesRestfulApiApplication.class, args);
+	}
+
+}
